656855378953921fb419a178b52beb580801ed29
louis <louis.milhaud@universite-paris-saclay.fr>
2024-03-12 16:45:20 +0100

    up

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from Graph import Graph, determine_edge_frequency
 import matplotlib.pyplot as plt
 from utils import cpt_cuts_correlation, preprocessing
 import json
+import geopandas
 
 def main():
     # a excute a partir du repo Casser_graphe (chemin relatifs)
@@ -13,19 +14,22 @@ def main():
     save_path = "./data/betweenness_Paris.json"
     print("import graphs...")
     G_nx = ox.load_graphml(grahml_path)
-    print(f"preprocessing the graph...")
-    preprocessing(G_nx, val="width")
-    print(f"Conversion into KaHIP format...")
-    G_kp = Graph(nx=G_nx)
-    print("compute betweenness...")
-    betweenness = G_kp.compute_edge_betweenness()
-    with open(save_path, "w") as save_file:
-        json.dump(betweenness, save_file)
-    print("import cuts...")
-    with open(json_path, "r") as read_file:
-        cuts = json.load(read_file)
-    print("compute frequency...")
-    freq = determine_edge_frequency(G_kp, cuts)
-    print(cpt_cuts_correlation(freq, betweenness))
+    preprocessing(G_nx)
+    gdf_edges, gdf_vertices = ox.graph_to_gdfs(G_nx)
+    gdf_edges.loc[gdf_edges.index.get_level_values('key') == 0]
+    # print(f"preprocessing the graph...")
+    # preprocessing(G_nx, val="width")
+    # print(f"Conversion into KaHIP format...")
+    # G_kp = Graph(nx=G_nx)
+    # print("compute betweenness...")
+    # betweenness = G_kp.compute_edge_betweenness()
+    # with open(save_path, "w") as save_file:
+    #     json.dump(betweenness, save_file)
+    # print("import cuts...")
+    # with open(json_path, "r") as read_file:
+    #     cuts = json.load(read_file)
+    # print("compute frequency...")
+    # freq = determine_edge_frequency(G_kp, cuts)
+    # print(cpt_cuts_correlation(freq, betweenness))
 
 main()
