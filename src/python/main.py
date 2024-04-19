81eed9194eebe60c8bae87a91cefba966873cb37
Louis MILHAUD <louis.milhaud@lip6.fr>
2024-04-19 10:34:58 +0200

    sync

--- src/python/main.py
+++ src/python/main.py
@@ -1,9 +1,8 @@
 from Graph import Graph
-from paths import graphml_path, kp_paths, rpaths_bc
-from visual import visualize_bc
+from paths import graphml_path, kp_paths
+from robustness import attack, extend_attack
 
 import json
-import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 from sys import setrecursionlimit
@@ -11,10 +10,7 @@ from sys import setrecursionlimit
 setrecursionlimit(100000)
 
 def main():
-    with open(rpaths_bc[0], "r") as bc_lanes_file:
-        bc_lanes = json.load(bc_lanes_file)
-    with open(rpaths_bc[1], "r") as bc_nocost_file:
-        bc_nocost = json.load(bc_nocost_file)
-    G = ox.load_graphml(graphml_path[0])# nocost
-    visualize_bc(bc_nocost, G, "presentations/images/visu_bc_nocost.pdf")
+    G = Graph(json=kp_paths[1]) # no cost
+    n = 5
+    attack(G, n, "data/nocost_graph_RD_"+str(n)+".json", "rd", True, True)
 main()
--- src/python/robustness.py
+++ src/python/robustness.py
@@ -6,6 +6,7 @@ import random as rd
 import networkx as nx
 import numpy as np
 from typing import Any
+from copy import deepcopy
 
 def freq_attack(G: Graph, ncuts: int) -> Edge:
     cut_union = []
@@ -26,36 +27,35 @@ def freq_attack(G: Graph, ncuts: int) -> Edge:
             frequencies[edge] = 1
     return max(frequencies, key=frequencies.get)
 
-def betweenness_attack(G: Graph, subset: list[Edge] | None) -> Edge:
+def betweenness_attack(G: Graph) -> Edge:
     bc = G.get_edge_bc()
-    key = subset if subset else bc.get # to test
-    return max(bc, key=key)
+    return max(bc, key=bc.get)
 
-def random_attack(G: Graph, n: int, subset: list[Edge] | None) -> list[Edge]:
-    edges = subset if subset else G._nx.edges
-    return rd.choices(edges, k=n)
+def random_attack(G: Graph, n: int) -> list[Edge]:
+    return rd.choices(list(G._nx.edges), k=n)
 
-def maxdegree_attack(G: Graph, subset: list[Edge] | None) -> Edge:
+def maxdegree_attack(G: Graph) -> Edge:
     maxdegree, chosen_edge = 0, None
-    edges = subset if subset else G._nx.edges
-    for edge in edges:
+    for edge in G._nx.edges:
         degree = G._nx.degree[edge[0]] * G._nx.degree[edge[1]]
         maxdegree = maxdegree if maxdegree > degree else degree
         chosen_edge = edge
     return chosen_edge
 
 def attack(
-    G: Graph, k: int, fp_save: str, order: str, metric_bc: bool, metric_cc: bool, ncuts: int = 1000, nrandoms: int = 100, save: bool = True, subset: list[Edge] | None = None
+    G: Graph, k: int, fp_save: str, order: str, metric_bc: bool, metric_cc: bool, ncuts: int = 1000, nrandoms: int = 100, save: bool = True
 ) -> RobustList | None:
     """
-    Simulates an attack on a Graph with the different strategies.
-    If subset isn't None, then  only edges in subset are considered when chosing edges.
-    If subset then frequency attack not available (will raise exception)
+    Simulates an attack on a Graph with the following strategy:
+        repeat k times:
+        - cuts ncuts times G
+        - remove the most cut edge
+
     Parameters:
         G: Graph
         k: int, the number of edges to remove
         fp_save: str, the saving path
-        order: str, can be "bc", "freq", "deg" or "rd" depending on the strategy to apply for the attack
+        order: str, can be "bc", "freq" or "rd" depending on the strategy to apply for the attack
             (ex. bc will remove the highest Betweenness Centrality edge)
         metric_bc: bool, whether the Betweenness Centrality is computed at each step
         metric_cc: bool, whether the max size of the connected components is computed or not
@@ -67,49 +67,54 @@ def attack(
          - removed edge
          - 0, 1 or 2 metrics applied to the graph at this step
     """
-    def not_rd_procedure(metrics):
+    def not_rd_procedure(metrics, chosen_edge):
         bc = G.get_edge_bc(new=True) if metric_bc else None
         cc = G.get_size_biggest_cc if metric_cc else None
         metrics.append((chosen_edge, bc, cc))
 
-    def rd_procedure(metrics):
+    def rd_procedure(metrics, chosen_edges):
         if len(chosen_edges) > 0:
             bcs, cc_list = [], []
-            for edge in range(chosen_edges):
-                G_copy = G.copy()
+            for edge in chosen_edges:
+                G_copy = deepcopy(G)
                 G_copy.remove_edge(edge)
-                bcs.append(G_copy.get_edge_bc(new=True))
-                cc_list.append(G_copy.get_biggest_connected_component)
-            metrics.append((chosen_edges, list(np.mean(bcs)), cc_list))
+                bcs.append(np.mean(list(G_copy.get_edge_bc(new=True).values())))
+                cc_list.append(G_copy.get_size_biggest_cc)
+            metrics.append((chosen_edges, bcs, cc_list))
         else:
-            not_rd_procedure(metrics)
-    if order == "freq" and not subset:
-        raise ValueError("subset must be None when frequency attack is chosen")
+            metrics.append((None, np.mean(list(G.get_edge_bc(new=True).values())), G.get_size_biggest_cc))
     metrics, chosen_edge, chosen_edges = [], None, []
     for i in range(k):
-        print(f"processing the {i}-th attack over {k}, order: {order}")
+        print(f"processing the {i+1}-th attack over {k}, order: {order}")
         match order:
             case "bc":
-                not_rd_procedure(metrics)
-                chosen_edge = betweenness_attack(G, subset)
+                not_rd_procedure(metrics, chosen_edge)
+                chosen_edge = betweenness_attack(G)
                 G.remove_edge(chosen_edge)
             case "freq":
-                not_rd_procedure(metrics)
+                not_rd_procedure(metrics, chosen_edge)
                 chosen_edge = freq_attack(G, ncuts)
                 G.remove_edge(chosen_edge)
-            case "deg":
-                not_rd_procedure(metrics)
-                chosen_edge = maxdegree_attack(G, subset)
-                G.remove_edge(chosen_edge)
             case "rd":
-                rd_procedure(metrics)
-                chosen_edges = random_attack(G, nrandoms, subset)
+                rd_procedure(metrics, chosen_edges)
+                chosen_edges = random_attack(G, nrandoms)
     if order != "rd":
-        pass
-        not_rd_procedure(metrics)
+        not_rd_procedure(metrics, chosen_edge)
     else:
-        rd_procedure(metrics)
+        rd_procedure(metrics, chosen_edges)
     if save:
+        if order != "rd":
+            temp = metrics.copy()
+            metrics = []
+            for step in temp:
+                str_d = {str(k): v for k, v in step[1].items()}
+                metrics.append([str(step[0]), str_d, step[2]])
+        else:
+            temp = metrics.copy()
+            metrics = []
+            for step in temp:
+                edges = [str(e) for e in step[0]] if step[0] else None
+                metrics.append([edges, step[1], step[2]])
         with open(fp_save, "w") as save_file:
             json.dump(metrics, save_file)
     else:
