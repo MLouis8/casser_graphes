e4b2c59a2ad480a79c79d8f2263b780e19d2df09
Louis MILHAUD <louis.milhaud@lip6.fr>
2024-05-02 13:21:01 +0200

    correct mistakes for khiva

--- src/python/main.py
+++ src/python/main.py
@@ -20,7 +20,7 @@ def main():
     with open("data/cuts/lanes_1000_005.json", "r") as read_file:
         data = json.load(read_file)
     # cut = data["141"] # 24
-    # G = Graph(json=kp_paths[9])
+    G = Graph(json=kp_paths[9])
     # G.set_last_results(cut[0], cut[1])
     # edges = G.process_cut()
     # attack on best cut types
@@ -28,7 +28,7 @@ def main():
     # attack(G, n, "lanes_graph_rd_" + str(n) + ".json", "rd", True, True, nrandoms=1)
     with open("data/robust/lanes_graph_deg_25.json", "r") as read_file:
         metrics = json.load(read_file)
-    extend_attack(G_nx, metrics, n, "lanes_graph_deg_" + str(n+25) + ".json", "deg", True, True, 1000, 100, True)
+    extend_attack(G, metrics, n, "lanes_graph_deg_" + str(n+25) + ".json", "deg", True, True, 1000, 100, True)
     
     # create visubcs
     # for i in range(11, 26):
